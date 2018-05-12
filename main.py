0b2a7f662ba425a9079463bcb0c1fca5647beb67
Johannes Hoppe <info@johanneshoppe.com>
2018-05-12 19:48:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import logging
 import sys
 
-from lintipy import Handler
+from lintipy import CheckRun
 
 root_logger = logging.getLogger('')
 root_logger.setLevel(logging.DEBUG)
@@ -11,7 +11,7 @@ root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
 def handle(*args, **kwargs):
     """Handle that is invoked by AWS lambda."""
-    Handler(
+    CheckRun(
         'bandit',
         'bandit.cli.main', '-r', '.'
     )(*args, **kwargs)
