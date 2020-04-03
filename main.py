cce61c137564500f6c51e88572a2665abbe8dc92
Johannes Hoppe <info@johanneshoppe.com>
2020-04-03 16:08:47 +0200

    Remove explicit logger setup

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 """Lambda function that executes bandit, a static file linter."""
-import logging
-import sys
-
 from lintipy import CheckRun
-
-root_logger = logging.getLogger('')
-root_logger.setLevel(logging.DEBUG)
-root_logger.addHandler(logging.StreamHandler(sys.stdout))
 
 
 handle = CheckRun.as_handler(
